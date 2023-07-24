2ada9b474d70629512ef1b5c39af6a67f824e71a
miguelgonzalez3 <miguel.gonzalez3@udc.es>
2023-07-24 18:07:31 +0200

    Cambio del valor del property excelPath

--- src/main/java/com/fileupload/web/app/AlfrescoFileUploadApplication.java
+++ src/main/java/com/fileupload/web/app/AlfrescoFileUploadApplication.java
@@ -1,5 +1,7 @@
 package com.fileupload.web.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -9,7 +11,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("com.fileupload.web.app.repository")
 // @EnableSwagger2
 public class AlfrescoFileUploadApplication {
+
+	static Logger logger = LoggerFactory.getLogger(AlfrescoFileUploadApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Estas usando la version 1.4");
 		SpringApplication.run(AlfrescoFileUploadApplication.class, args);
 	}
+	
 }
